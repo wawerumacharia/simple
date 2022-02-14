b83cb613f4e285bf895e8a1359808567f360f595
wawerumacharia <97959618+wawerumacharia@users.noreply.github.com>
2022-02-14 16:41:31 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,56 @@
+/*
+simple calculator
+by geoffrey macharia
+on feb,2022
+MIT Licence
+c89 compailer
+*/
+#include <stdlib.h>
+
+int main()
+{//variable declaration
+    int n1,n2,sm,diff,product;
+    float quot;
+    printf("simple calculator!\n");
+    prinnt f(enter yor name)
+    gets (name)
+    prnt f(enter two integers)
+    scanf(*|add'nlcn2)
+    print f (enter two integers:);
+    scan f(;)
+    scan
+   int n1,n2,sm,diff,product;
+    float quot;
+    printf("smple calculator!\n");
+    prinnt f(eff mash)
+    gets (name)
+    prnt f(enter two integers)
+    scanf(*|add'nlcn2)
+    print f (enter two integers:);
+    scan f(;)
+    scan
+   int n1,n2,sm,diff,product;
+    float quot;
+    printf("smple calculator!\n");
+    prinnt f(eff mash)
+    gets (name)
+    prnt f(enter two integers)
+    scanf(*|add'nlcn2)
+    print f (enter two integers:);
+    scan f(;)
+    scan
+   int n1,n2,sm,diff,product;
+    float quot;
+    printf("smple calculator!\n");
+    prinnt f(eff mash)
+    gets (name)
+    prnt f(enter two integers)
+    scanf(*|add'nlcn2)
+    print f (enter two integers:);
+    scan f(;)
+    scan
+
+
+
+
+
